1bc7296b39e55c801bee6db69ef0e245525031c4
Endy Muhardin <endy.muhardin@gmail.com>
2017-09-25 11:09:52 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/AplikasiKompensasiApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiKompensasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiKompensasiApplication.class, args);
+	}
+}
