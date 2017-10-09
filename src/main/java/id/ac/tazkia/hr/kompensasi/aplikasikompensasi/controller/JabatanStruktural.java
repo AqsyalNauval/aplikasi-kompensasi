b20dca1865c5ffd26fbf55f68eb2a003bbd07737
gifar <gifar@tazkia.ac.id>
2017-10-09 15:31:24 +0700

    buat screen jabatab struktural

--- src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/JabatanStruktural.java
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/JabatanStruktural.java
@@ -1,4 +1,19 @@
 package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class JabatanStruktural {
+
+    @GetMapping("/jabatan_struktural/daftar_jabatan_struktural")
+    public void daftarJabatanStruktural(){
+
+
+    }
+
+    @GetMapping("/jabatan_struktural/form_jabatan_struktural")
+    public void formJabatanStruktural(){
+
+    }
 }
