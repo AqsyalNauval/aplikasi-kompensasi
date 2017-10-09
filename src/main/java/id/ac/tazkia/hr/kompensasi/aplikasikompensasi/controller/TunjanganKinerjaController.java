337da775f90f38b8c8adf8ab5202b37991dddada
Haffizh <haffizh@tazkia.ac.id>
2017-10-09 15:42:23 +0700

    bikin screen gaji pokok

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/TunjanganKinerjaController.java
@@ -0,0 +1,17 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TunjanganKinerjaController {
+
+    @GetMapping("/tunjangan_kinerja/list")
+    public void daftarTunjanganKinerja(){
+
+    }
+    @GetMapping("/tunjangan_kinerja/form")
+    public void tampilkanForm(){
+
+    }
+}
