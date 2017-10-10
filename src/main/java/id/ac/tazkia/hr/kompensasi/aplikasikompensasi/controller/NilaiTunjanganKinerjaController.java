461e1d328b523a3867a2fd10f9894f06cc68a532
Sawrain <ramadhani@tazkia.ac.id>
2017-10-10 14:38:47 +0700

    buat screen nilai tunjangan

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/NilaiTunjanganKinerjaController.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NilaiTunjanganKinerjaController {
+
+    @GetMapping("/nilai_tunjangan_kinerja/form_nilai_tunjangan")
+    public void daftarTunjanganKinerja(){
+
+    }
+}
