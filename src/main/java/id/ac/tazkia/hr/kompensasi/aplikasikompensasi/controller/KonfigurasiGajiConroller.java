105ac326f7cdb34ece69a789f9efcbcea68e7b13
Haffizh <haffizh@tazkia.ac.id>
2017-10-10 10:34:53 +0700

    Buat screen konfigurasi gaji karyawan

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/KonfigurasiGajiConroller.java
@@ -0,0 +1,17 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KonfigurasiGajiConroller {
+
+    @GetMapping("/konfigurasi_gaji_karyawan/list")
+    public void list(){
+
+    }
+    @GetMapping("/konfigurasi_gaji_karyawan/form")
+    public void form(){
+
+    }
+}
