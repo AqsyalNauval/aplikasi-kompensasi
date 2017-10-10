f40de2c4278456a04003093211484eeeff308ccc
gifar <gifar@tazkia.ac.id>
2017-10-10 10:34:51 +0700

    buat screen entry kegiatan karyawan

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/EntryKegiatanKaryawan.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EntryKegiatanKaryawan {
+
+    @GetMapping("/entry_kegiatan_karyawan/form")
+    public void formEntryKegiatanKaryawan(){
+
+    }
+}
