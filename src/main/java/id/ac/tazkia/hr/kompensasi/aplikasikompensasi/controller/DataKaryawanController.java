81b3722dba83e79fec71634955fbf07c1a5b5344
Sawrain <ramadhani@tazkia.ac.id>
2017-10-11 10:03:42 +0700

    form data karyawan

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/DataKaryawanController.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DataKaryawanController {
+
+    @GetMapping("/data_karyawan/data_karyawan")
+    public void daftarDepartemen(){
+
+    }
+}
