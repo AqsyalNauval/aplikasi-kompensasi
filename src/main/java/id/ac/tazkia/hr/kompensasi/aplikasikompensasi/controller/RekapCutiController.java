7a8dcfedfa4ee1aea0fb0cad5d2807c1b9f67b55
Sawrain <ramadhani@tazkia.ac.id>
2017-10-16 13:40:00 +0700

    screen rekap cuti

---
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/RekapCutiController.java
@@ -0,0 +1,13 @@
+package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RekapCutiController {
+
+    @GetMapping("/cuti/list")
+    public void rekapCutiKinerja(){
+
+    }
+}
