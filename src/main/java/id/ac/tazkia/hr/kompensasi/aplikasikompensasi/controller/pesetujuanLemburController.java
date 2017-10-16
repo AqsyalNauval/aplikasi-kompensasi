9bf59b5c5cb164d1942be438091bfb587a70f9f5
Haffizh <haffizh@tazkia.ac.id>
2017-10-16 10:15:14 +0700

    screen persetujuan lembur

--- src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/pesetujuanLemburController.java
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/pesetujuanLemburController.java
@@ -1,4 +1,12 @@
 package id.ac.tazkia.hr.kompensasi.aplikasikompensasi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class pesetujuanLemburController {
+    @GetMapping("/lembur/approval/list")
+    public void list(){
+
+    }
 }
