1d4c6f722b2eb8e5424539100396a8dfb01d7926
Haffizh <haffizh@tazkia.ac.id>
2017-10-16 10:27:39 +0700

    screan persetujuan cuti

--- src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/PesetujuanLemburController.java
+++ src/main/java/id/ac/tazkia/hr/kompensasi/aplikasikompensasi/controller/PesetujuanLemburController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class pesetujuanLemburController {
+public class PesetujuanLemburController {
     @GetMapping("/lembur/approval/list")
     public void list(){
 
